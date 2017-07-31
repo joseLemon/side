e0bd587ea0188e5cab49b29c19183d77a17300cf
José Angel Lujan Villaseñor <jose.lujan@lemonthree.mx>
2017-07-31 12:25:15 -0600

    programas and info section

--- resources/views/site/micro.blade.php
+++ resources/views/site/micro.blade.php
@@ -146,6 +146,22 @@
                     </p>
                 </div>
             </div>
+            <div class="info">
+                <div class="row no-margin">
+                    <div class="col-sm-4">
+                        <img src="{{ asset('img/micro/acerca/1.png') }}">
+                        <h2>Misión</h2>
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="{{ asset('img/micro/acerca/2.png') }}">
+                        <h2>Visión</h2>
+                    </div>
+                    <div class="col-sm-4">
+                        <img src="{{ asset('img/micro/acerca/3.png') }}">
+                        <h2>Valores</h2>
+                    </div>
+                </div>
+            </div>
         </div>
         <!-- ================================== -->
 
@@ -185,23 +201,89 @@
                 </div>
             </div>
         </div>
-        <div class="programas" id="programas">
-            <div class="container spacing">
-                <h1 class="heading bold text-center">Conoce nuestros programas</h1>
-            </div>
-        </div>
         <!-- ================================== -->
 
         <!-- ///////////  PROGRAMAS  \\\\\\\\\\\ -->
 
         <!-- ================================== -->
         <div class="programas" id="programas">
+            <div class="container spacing top">
+                <h1 class="heading bold text-center">Conoce nuestros programas</h1>
+                <div class="row no-margin">
+                    <div class="col-sm-6">
+                        <div>
+                            <div class="img-container">
+                                <img src="{{ asset('img/micro/programas/1.png') }}">
+                            </div>
+                            <div class="text-container">
+                                <h4>Financiamiento para el desarrollo del Estado de Chihuahua</h4>
+                                <a href="">Descargar Solicitud</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div>
+                            <div class="img-container">
+                                <img src="{{ asset('img/micro/programas/2.png') }}">
+                            </div>
+                            <div class="text-container">
+                                <h4>Programa del fondo de las actividades productivas</h4>
+                                <a href="">Descargar Solicitud</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row no-margin">
+                    <div class="col-sm-6">
+                        <div>
+                            <div class="img-container">
+                                <img src="{{ asset('img/micro/programas/3.png') }}">
+                            </div>
+                            <div class="text-container">
+                                <h4>Fondo de apoyo al Desarrollo Social</h4>
+                                <a href="">Descargar Solicitud</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div>
+                            <div class="img-container">
+                                <img src="{{ asset('img/micro/programas/4.png') }}">
+                            </div>
+                            <div class="text-container">
+                                <h4>Financiamiento para el desarrollo de Ciudad Juárez</h4>
+                                <a href="">Descargar Solicitud</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div class="parallax-container">
                 <div class="parallax">
                     <img src="{{ asset('img/index/banners/6.jpg') }}">
                 </div>
-                <div class="container">
+                <div class="container bottom">
                     <h1 class="heading bold white text-center">Programas administrados</h1>
+                    <div class="row no-margin text-center">
+                        <div class="col-sm-6">
+                            <div>
+                                <div class="img-container">
+                                    <img src="{{ asset('img/micro/programas/5.png') }}">
+                                </div>
+                                <h4>Programa de apoyo e innovación tecnológica para la micro y pequeña empresa</h4>
+                                <a href="">Descargar Solicitud</a>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div>
+                                <div class="img-container">
+                                    <img src="{{ asset('img/micro/programas/6.png') }}">
+                                </div>
+                                <h4>Programa de fomento al desarrollo turístico</h4>
+                                <a href="">Descargar Solicitud</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
